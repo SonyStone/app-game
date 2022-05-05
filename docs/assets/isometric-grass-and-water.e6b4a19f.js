0e3f1980a4bfd41dc391070682c966d90de40e53
iliaverpa <iliaverpa@gmail.com>
2022-05-06 02:30:58 +0300

    remove assets folder

--- docs/assets/isometric-grass-and-water.e6b4a19f.js
+++
@@ -1,1 +0,0 @@
-var s="/assets/isometric-grass-and-water.735a3c1b.json",a="/assets/isometric-grass-and-water.8b53306d.png",r={map:s,tiles:a};export{r as i};
---
+++ docs/isometric-grass-and-water.e6b4a19f.js
@@ -0,0 +1,1 @@
+var s="/isometric-grass-and-water.735a3c1b.json",a="/isometric-grass-and-water.8b53306d.png",r={map:s,tiles:a};export{r as i};
