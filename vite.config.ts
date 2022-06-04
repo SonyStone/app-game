67a79de21eaeade3956b354595544635eb341c4f
iliaverpa <iliaverpa@gmail.com>
2022-06-04 06:56:37 +0300

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,19 @@ export default defineConfig({
       '@webgl': resolve(__dirname, './src/libs/webgl'),
     },
   },
+  test: {
+    environment: 'jsdom',
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    // solid needs to be inline to work around
+    // a resolution issue in vitest:
+    deps: {
+      inline: [/solid-js/],
+    },
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    // threads: false,
+    // isolate: false,
+  },
 });
