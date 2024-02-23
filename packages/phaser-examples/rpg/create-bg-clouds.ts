ae0bf0b800699697544f14246fd8f3ecda00bd3a
ilyav <ilyave@matrix.co.il>
2024-02-23 04:53:13 +0100

    fix: firefox

--- packages/phaser-examples/rpg/create-bg-clouds.ts
+++ packages/phaser-examples/rpg/create-bg-clouds.ts
@@ -15,28 +15,30 @@ export function createBGClouds(props: {
   tweens: Phaser.Tweens.TweenManager;
 }) {
   const bgClouds = [BG_CLOUD2, BG_CLOUD1];
-  bgClouds.map((key, index) => {
-    const tileSprite = props.make.tileSprite({
-      x: 0,
-      y: 800,
-      width: 176 * 1000,
-      height: 176,
-      key: key
+  try {
+    bgClouds.map((key, index) => {
+      const tileSprite = props.make.tileSprite({
+        x: 0,
+        y: 800,
+        width: 176 * 1000,
+        height: 176,
+        key: key
+      });
+      tileSprite.scale = 3;
+      tileSprite.scrollFactorY = (index + 1) * 0.25;
+      tileSprite.scrollFactorX = (index + 1) * 0.25;
+      tileSprite.tilePositionX = 100;
+      tileSprite.setDepth(-2);
+      const tween = props.tweens.add({
+        targets: tileSprite,
+        tilePositionX: { from: 0, to: 176 },
+        ease: 'liner',
+        duration: 100000 + index * 5000,
+        repeat: -1
+      });
+      return tileSprite;
     });
-    tileSprite.scale = 3;
-    tileSprite.scrollFactorY = (index + 1) * 0.25;
-    tileSprite.scrollFactorX = (index + 1) * 0.25;
-    tileSprite.tilePositionX = 100;
-    tileSprite.setDepth(-2);
-
-    const tween = props.tweens.add({
-      targets: tileSprite,
-      tilePositionX: { from: 0, to: 176 },
-      ease: 'liner',
-      duration: 100000 + index * 5000,
-      repeat: -1
-    });
-
-    return tileSprite;
-  });
+  } catch (e) {
+    console.error(e);
+  }
 }
